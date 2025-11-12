3291f13984986095e6648c15464374f1e76daf96
Yuzaki Alfarishi <yuzakialfarishi29@gmail.com>
2025-11-12 10:37:56 +0700

    Upload awal proyek Tastyfood

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home')->name('home');
+Route::view('/tentang', 'tentang')->name('tentang');
+Route::view('/berita', 'berita')->name('berita');
+Route::view('/galeri', 'galeri')->name('galeri');
+Route::view('/kontak', 'kontak')->name('kontak');
